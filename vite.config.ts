3d6a3d122a954682a4a70fe34d2893f7217311c4
Weslei França <weslwifranca@gmail.com>
2024-12-03 17:11:41 -0300

    add swc/plugin-styled-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-styled-components'],
+      },
+    }),
     legacy({
       targets: ['defaults', 'not IE 11'],
       renderLegacyChunks: false,
